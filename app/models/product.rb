189780c57fb526c7fc411d5ea32dfc240e570484
TdeSouzaRRC <tdesouza@academic.rrc.ca>
2017-11-13 01:31:47 +0000

    Added large size to paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-    has_attached_file :product_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :product_image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :product_image, content_type: /\Aimage\/.*\z/
 
     validates :name, :description, :stock_quantity, :price, :product_image, presence: true
