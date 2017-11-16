4d5d79d325dc76b42eab86f2dadd45bd3456cede
TdeSouzaRRC <tdesouza@academic.rrc.ca>
2017-11-16 22:37:11 +0000

    Created some routes, views, and home controller action to handle checkoutprocess

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   before_action :initialize_session, only: [:index]
-
+  
   def index
     @categories = Category.all.order(:name)
     @selected_category = nil;
@@ -26,6 +26,26 @@ class HomeController < ApplicationController
 
   def shopping_cart
     @products = Product.find(session[:cart])
+  end
+
+  def summary_build
+    line_items = []
+
+    params[:line_item].each do |item|
+      product = Product.find(item["product_id"])
+
+      unless(product.nil?) then
+        temp_line_item = LineItem.new
+        temp_line_item.quantity = item["quantity"]
+        temp_line_item.price = product.price * temp_line_item.quantity
+        temp_line_item.product_id = product.id
+        line_items << temp_line_item
+      end
+    end
+    
+    session[:line_items] = line_items
+    
+    redirect_to checkout_path
   end
 
   private 
